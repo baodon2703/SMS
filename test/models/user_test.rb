2813d22627e8c339b17ba5e7f1a9bd6265e8fd43
Don Nguyen <baodon2703@gmail.com>
2018-05-03 16:50:43 +0900

    Implement advance login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -65,10 +65,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-  # test "email addresses should be saved as lower-case" do
-  #   mixed_case_email = "Foo@ExAMPle.CoM"
-  #   @user.email = mixed_case_email
-  #   @user.save
-  #   assert_equal mixed_case_email.downcase, @user.reload.email
-  # end
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticate?('')
+  end
 end
